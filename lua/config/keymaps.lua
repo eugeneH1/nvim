5b1f749673656486db4a3477254fd665b8f85768
Eugene Heynike <eugeneheynike@Eugenes-MacBook-Pro.local>
2025-06-15 13:34:25 +0200

    latest config that's working. maybe question tokyonight config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 
-vim.keymap.set({ "i", "v" }, "jk", "<Esc>", { silent = true })
-vim.keymap.set({ "i", "v" }, "kj", "<Esc>", { silent = true })
+vim.keymap.set({ "i", "v" }, "df", "<Esc>", { noremap = true, silent = true })
+-- vim.keymap.set({ "i", "v" }, "jj", "<Esc>", { noremap = true, silent = true })
