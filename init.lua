669de2fe11f720af782e0a540c8fa42e6af586ea
Eugene Heynike <eugeneheynike@gmail.com>
2026-03-24 07:58:10 +0200

    updates

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- ~/.config/nvim/init.lua
+
+-- Always load LazyVim so it can process your plugins and the VSCode Extra
+require("config.lazy")
+
 if vim.g.vscode then
-  -- VSCode extension
-else
-  -- ordinary Neovim
-  require("config.lazy")
+  -- Add overrides that you ONLY want in VS Code here
+  -- Example: your 'df' to 'esc' mapping
+  vim.keymap.set("i", "df", "<Esc>", { silent = true })
 end
